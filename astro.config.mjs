e0c4add3dfb0c41ac72d4202e87861d355a907e3
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2024-04-08 21:20:13 +0700

    update node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import playformCompress from "@playform/compress";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
